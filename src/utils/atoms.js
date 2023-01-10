488e7a0a2f4041cbfbb42a2cba9b3cf85ec4c6a3
Honey171 <103601089+Honey171@users.noreply.github.com>
2023-01-10 18:06:31 +0300

    Create atoms.js

---
+++ src/utils/atoms.js
@@ -0,0 +1,42 @@
+import { atom } from 'recoil';
+
+export const productState = atom({
+  key: 'product',
+  default: false,
+});
+export const industriesState = atom({
+  key: 'industries',
+  default: false,
+});
+export const learningState = atom({
+  key: 'learning',
+  default: false,
+});
+export const supportState = atom({
+  key: 'support',
+  default: false,
+});
+export const companyState = atom({
+  key: 'company',
+  default: false,
+});
+export const salesforceState = atom({
+  key: 'salesforce',
+  default: false,
+});
+export const loginState = atom({
+  key: 'login',
+  default: false,
+});
+export const contactState = atom({
+  key: 'contact',
+  default: false,
+});
+export const regionState = atom({
+  key: 'region',
+  default: false,
+});
+export const anyModalOpenedState = atom({
+  key: 'modals',
+  default: false,
+});
