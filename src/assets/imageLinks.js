653f2b39d0fc8f5be81bbbd58c175d74819f51fd
Honey171 <103601089+Honey171@users.noreply.github.com>
2023-01-10 18:07:03 +0300

    Add files via upload

---
+++ src/assets/imageLinks.js
@@ -0,0 +1,92 @@
+// Navbar
+
+export const logo =
+  'https://c1.sfdcstatic.com/content/dam/sfdc-docs/www/logos/logo-salesforce.svg';
+export const miniLogo =
+  'https://a.sfdcstatic.com/shared/images/c360-nav/cloud.svg';
+export const salesforceHamburger =
+  'https://a.sfdcstatic.com/shared/images/c360-nav/icons.svg#hamburger-blue';
+export const salesforceClose =
+  'https://a.sfdcstatic.com/shared/images/c360-nav/icons.svg#close-blue';
+
+// Hero section
+
+export const wheel =
+  'https://www.salesforce.com/content/dam/web/en_ie/www/images/homepage/homepage-post-df-cost-savings-fg-new.png';
+
+// New section
+
+export const imgNewOne =
+  'https://www.salesforce.com/content/dam/web/en_us/www/images/platform/overview/whats-new-genie.png';
+export const imgNewTwo =
+  'https://www.salesforce.com/content/dam/web/fr_fr/www/images/state-of-connected-customer-110x110-min.png';
+export const imgNewThree =
+  'https://www.salesforce.com/content/dam/web/en_ie/www/images/homepage/cust-xp-smb-hp-newsbar-110x110-emea.png';
+
+// Product section
+
+export const imgProductOne =
+  'https://www.salesforce.com/content/dam/web/en_us/www/images/home/php-products-4.png';
+export const imgProductTwo =
+  'https://www.salesforce.com/content/dam/web/en_us/www/images/home/product-sales.png';
+export const imgProductThree =
+  'https://www.salesforce.com/content/dam/web/en_us/www/images/home/product-service.png';
+export const imgProductFour =
+  'https://www.salesforce.com/content/dam/web/en_us/www/images/home/php-products-7.png';
+
+// Content section
+
+export const imgContentOne =
+  'https://www.salesforce.com/content/dam/web/en_us/www/images/home/php-rc-card-1-2.png';
+export const imgContentTwo =
+  'https://www.salesforce.com/content/dam/web/en_us/www/images/home/php-rc-card-2.png';
+export const imgContentThree =
+  'https://www.salesforce.com/content/dam/web/en_us/www/images/home/php-rc-smb-navigating-through-change.jpg';
+export const imgContentFour =
+  'https://www.salesforce.com/content/dam/web/en_us/www/images/home/php-rc-card-4.png';
+
+// Event section
+
+export const imgEventOne =
+  'https://www.salesforce.com/content/dam/web/en_us/www/images/home/php-events-1.png';
+export const imgEventTwo =
+  'https://www.salesforce.com/content/dam/web/en_ie/www/images/homepage/405x219px.png';
+export const imgEventThree =
+  'https://www.salesforce.com/content/dam/web/en_us/www/images/home/php-events-3.png';
+
+// Skills section
+
+export const imgSkillsOne =
+  'https://www.salesforce.com/content/dam/web/en_us/www/images/home/php-trailhead-1.png';
+export const imgSkillsTwo =
+  'https://www.salesforce.com/content/dam/web/en_us/www/images/home/php-trailhead-2.png';
+export const imgSkillsThree =
+  'https://www.salesforce.com/content/dam/web/en_us/www/images/home/php-trailhead-3.png';
+
+// Discover section
+
+export const imgDiscover =
+  'https://www.salesforce.com/content/dam/web/en_us/www/images/home/appexchange-promo-fg.png';
+
+// Install section
+
+export const imgInstallDesktop =
+  'https://www.salesforce.com/content/dam/web/en_ie/www/images/homepage/php-freetrial-eu-footer-dsktop.png';
+export const imgInstallMobile =
+  'https://www.salesforce.com/content/dam/web/en_ie/www/images/homepage/php-freetrial-eu-footer-mob.png';
+
+// Socials
+
+export const fb =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/facebook-day.svg?version=202300';
+export const tw =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/twitter-day.svg?version=202300';
+export const lin =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/linkedin-day.svg?version=202300';
+export const yt =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/youtube-day.svg?version=202300';
+
+// Fixed chat
+
+export const femalePhoto =
+  'https://www.salesforce.com/content/dam/web/en_us/shared/pilot/female-1-small@2x.jpg';
---
+++ src/assets/mobileNavbarImageLinks.js
@@ -0,0 +1,62 @@
+// Products
+
+export const productOne =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/placeholder11.svg';
+export const productTwo =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/placeholder29.svg';
+export const productThree =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/sales-day.svg';
+export const productFour =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/service-day.svg';
+export const productFive =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/marketing-day.svg';
+export const productSix =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/commerce-day.svg';
+export const productSeven =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/analytics-day.svg';
+export const productEight =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/integration-day.svg';
+export const productNine =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/placeholder13.svg';
+export const productTen =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/platform-day.svg';
+export const productEleven =
+  'https://www.salesforce.com/content/dam/web/global/icons/product/products-sustainability-day.svg';
+export const productTwelve =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/grid-day.svg';
+export const productThirteen =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/calendar-day.svg';
+
+// Expand
+
+export const expand =
+  'https://a.sfdcstatic.com/shared/images/c360-nav/link.svg';
+
+// Industries
+
+export const industriesOne =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/placeholder16.svg';
+export const industriesTwo =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/events-day.svg';
+export const industriesThree =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/consumer-goods-day.svg';
+export const industriesFour =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/placeholder20.svg';
+export const industriesFive =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/placeholder21.svg';
+export const industriesSix =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/financial-day.svg';
+export const industriesSeven =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/health-day.svg';
+export const industriesEight =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/manufacturing-day.svg';
+export const industriesNine =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/placeholder30.svg';
+export const industriesTen =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/placeholder23.svg';
+export const industriesEleven =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/radio-day.svg';
+export const industriesTwelve =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/placeholder25.svg';
+export const industriesThirteen =
+  'https://www.salesforce.com/content/dam/web/global/svg-icons/placeholder27.svg';
